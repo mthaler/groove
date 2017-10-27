8834782a69747c79203d3f2f411ccd24e156dd56
Michael Thaler <michael.thaler@dlr.de>
2017-10-27 10:40:29 +0200

    Implemented GridBagConstraints.

--- src/main/scala/com/mthaler/groove/gridbaglayout/GridBagConstraints.scala
+++ src/main/scala/com/mthaler/groove/gridbaglayout/GridBagConstraints.scala
@@ -1,4 +1,20 @@
 package com.mthaler.groove.gridbaglayout
 
-case class GridBagConstraints()
+import java.awt.{GridBagConstraints => JGridBagConstraints}
+
+case class GridBagConstraints(gridx: Int = JGridBagConstraints.RELATIVE,
+                              gridy: Int = JGridBagConstraints.RELATIVE,
+                              gridwidth: Int = 1,
+                              gridheight: Int = 1,
+                              weightx: Double = 0.0,
+                              weighty: Double = 0.0,
+                              anchor: Anchor.Anchor = Anchor.CENTER,
+                              fill: Fill.Fill = Fill.NONE,
+                              insets: Insets = Insets.Empty,
+                              ipadx: Int = 0,
+                              ipady: Int = 0) {
+
+  def toAWT: JGridBagConstraints = new JGridBagConstraints(gridx, gridy, gridwidth, gridheight,
+    weightx, weighty, anchor.toInt, fill.toInt, insets.toAWT, ipadx, ipady)
+}
 
--- src/main/scala/com/mthaler/groove/gridbaglayout/Insets.scala
+++ src/main/scala/com/mthaler/groove/gridbaglayout/Insets.scala
@@ -6,3 +6,7 @@ case class Insets(top: Int, left: Int, bottom: Int, right: Int) {
 
   def toAWT: JInsets = new JInsets(top, left, bottom, right)
 }
+
+object Insets {
+  val Empty = Insets(0, 0, 0, 0)
+}
