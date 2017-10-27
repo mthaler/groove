e1e725f37562747aa2f93f1a5701a39cb8258cd5
Michael Thaler <michael.thaler@dlr.de>
2017-10-27 11:01:37 +0200

    Added SimpleGridBagLayoutBuilder.

---
+++ src/main/scala/com/mthaler/groove/gridbaglayout/SimpleGridBagLayoutBuilder.scala
@@ -0,0 +1,28 @@
+package com.mthaler.groove.gridbaglayout
+
+import javax.swing.JComponent
+
+import scala.collection.mutable.ArrayBuffer
+
+object SimpleGridBagLayoutBuilder {
+
+  case class Item(component: JComponent, constraints: GridBagConstraints)
+
+  case class Row(items: Seq[Item])
+
+  def row(items: Tuple2[JComponent, GridBagConstraints]*)(implicit b: SimpleGridBagLayoutBuilder) = {
+    b.addRow(Row(items.map { case(component, constraint) => Item(component, constraint) }))
+  }
+}
+
+
+class SimpleGridBagLayoutBuilder {
+
+  import SimpleGridBagLayoutBuilder._
+
+  val rows = ArrayBuffer.empty[Row]
+
+  def addRow(row: Row): Unit = {
+    rows += row
+  }
+}
