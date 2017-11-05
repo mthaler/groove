85564c30e234e7146485128067492573e0c8b7ee
Michael Thaler <michael.thaler@dlr.de>
2017-11-05 15:38:39 +0100

    More work on BoxBuilder.

--- src/main/scala/com/mthaler/groove/box/BoxBuilder.scala
+++ src/main/scala/com/mthaler/groove/box/BoxBuilder.scala
@@ -3,27 +3,53 @@ package com.mthaler.groove.box
 import java.awt.{Component, Dimension}
 import javax.swing.Box
 
-object BoxBuilder {
+import scala.collection.mutable.ArrayBuffer
 
-  def horizontalBox(components: Component*): Box = {
-    val b = Box.createHorizontalBox()
-    b
+trait BoxBuilder {
+
+  private val builder = ArrayBuffer.empty[Component]
+
+  def horizontalBox(body: => Unit): Box = {
+    body
+    val box = Box.createHorizontalBox()
+    for (component <- builder) box.add(component)
+    builder.clear()
+    box
   }
 
-  def verticalBox(components: Component*): Box = {
-    val b = Box.createVerticalBox()
-    b
+  def verticalBox(body: => Unit): Box = {
+    body
+    val box = Box.createVerticalBox()
+    for (component <- builder) box.add(component)
+    builder.clear()
+    box
   }
 
-  def glue: Component = Box.createGlue()
+  def glue: Unit = {
+    builder += Box.createGlue
+  }
 
-  def horizontalGlue: Component = Box.createHorizontalGlue()
+  def horizontalGlue: Unit = {
+    builder += Box.createHorizontalGlue()
+  }
 
-  def verticalGlue: Component = Box.createVerticalGlue()
+  def verticalGlue: Unit = {
+    builder += Box.createVerticalGlue()
+  }
 
-  def horizontalStrut(width: Int): Component = Box.createHorizontalStrut(width)
+  def horizontalStrut(width: Int): Unit = {
+    builder += Box.createHorizontalStrut(width)
+  }
 
-  def verticalStrut(height: Int): Component = Box.createVerticalStrut(height)
+  def verticalStrut(height: Int): Unit = {
+    builder += Box.createVerticalStrut(height)
+  }
 
-  def rigidArea(width: Int, height: Int): Component = Box.createRigidArea(new Dimension(width, height))
+  def rigidArea(width: Int, height: Int): Unit = {
+    builder += Box.createRigidArea(new Dimension(width, height))
+  }
+
+  def component(component: Component): Unit = {
+    builder += component
+  }
 }
--- src/test/scala/com/mthaler/groove/box/BoxBuilderTestApp.scala
+++ src/test/scala/com/mthaler/groove/box/BoxBuilderTestApp.scala
@@ -2,23 +2,22 @@ package com.mthaler.groove.box
 
 import java.awt.Dimension
 import javax.swing.{JCheckBox, JFrame, JLabel}
-import BoxBuilder._
 
-object BoxBuilderTestApp extends App {
+object BoxBuilderTestApp extends App with BoxBuilder {
   val frame = new JFrame("BoxBuilderTestApp")
   frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
 
   val label = new JLabel("test")
   val checkBox = new JCheckBox("check me")
 
-  val b = horizontalBox(
-    label,
-    horizontalGlue,
-    checkBox
-  )
+  val b = horizontalBox {
+    component(label)
+    horizontalGlue
+    component(checkBox)
+  }
 
   frame.setContentPane(b)
-  frame.setPreferredSize(new Dimension(200, 120))
+  frame.setPreferredSize(new Dimension(250, 36))
   frame.pack()
   frame.setVisible(true)
 }
